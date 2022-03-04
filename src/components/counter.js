d41d25295c5b913328565ade19a8088273df3e5a
Alex <exemple@gmail.com>
2022-03-04 18:07:42 +0200

    use hooks

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,29 +1,33 @@
 //import '../images';
 import React from 'react'
 //import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import * as actions from '../actions';
+import {connect, useSelector, useDispatch} from 'react-redux';
+//import * as actions from '../actions';
+import {inc,dec,rnd} from '../actions';
 import plus from '../images/plus.png';
 import minus from '../images/Minus.png';
 
 
 
-const Counter = ({count,inc,  dec,  rnd}) => {
+//const Counter = ({count,inc,  dec,  rnd}) => {
+const Counter = () => {
+    const count = useSelector(state => state);
+    const dispatch = useDispatch();
     return(
     <>
         <h1 className="d-flex justify-content-center">{count}</h1>
         <div className="d-flex justify-content-between but_group">
-            <button onClick={inc}  type="button" className="btn inc">
+            <button onClick={() => dispatch(inc())}  type="button" className="btn inc">
             <div> 
                 <img src={plus} alt="plus" />
             </div>
             </button>
-            <button  onClick={dec}  type="button" className='btn dec'>
+            <button  onClick={() => dispatch(dec())}  type="button" className='btn dec'>
             <div>
                 <img src={minus} alt="minus" />
             </div>
             </button>    
-            <button onClick={rnd} type="button" className='btn rnd'>
+            <button onClick={() => dispatch(rnd())} type="button" className='btn rnd'>
             <div >Rand</div>
             </button>    
         </div>
@@ -31,14 +35,15 @@ const Counter = ({count,inc,  dec,  rnd}) => {
     )
 }
 
-const mapStateToProps = state => {
-    return {
-        count : state
-    }
-}
+// const mapStateToProps = state => {
+//     return {
+//         count : state
+//     }
+// }
 
 // const mapDispatchToProps = dispatch => {
 //     return bindActionCreators(actions,dispatch)
 // }
 
-export default connect(mapStateToProps, actions)(Counter);
+//export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
